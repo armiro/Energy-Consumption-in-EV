06e5f78044b69c10a60ec7e95e0064aa98df2e72
armiro <arman.hgh.1374@gmail.com>
2019-03-31 17:55:39 +0430

    add KFold cross-validation

--- driving_range_prediction.py
+++ driving_range_prediction.py
@@ -21,16 +21,16 @@ new_path = "./data files/new_data.csv"
 
 
 """remove missing values (comment it after the first run)"""
-ds = pd.read_csv(filepath_or_buffer=old_path)
-ds = ds[pd.notnull(obj=ds['quantity(kWh)'])]
-ds = ds[pd.notnull(obj=ds['avg_speed(km/h)'])]
-ds.to_csv(path_or_buf=new_path)
+# ds = pd.read_csv(filepath_or_buffer=old_path)
+# ds = ds[pd.notnull(obj=ds['quantity(kWh)'])]
+# ds = ds[pd.notnull(obj=ds['avg_speed(km/h)'])]
+# ds.to_csv(path_or_buf=new_path)
 
 
 """load the data"""
 dataset = pd.read_csv(filepath_or_buffer=new_path)
 # print(dataset.head(n=5))
-print(dataset.describe())
+# print(dataset.describe())
 
 X = dataset.iloc[:, 5:14].values
 y = dataset.iloc[:, 14].values
@@ -55,7 +55,7 @@ X = onehot_encoder.fit_transform(X=X).toarray()
 X = X[:, 1:]
 
 # split the dataset into training-set and test-set
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=0)
 
 # scale the features
 sc = StandardScaler()
@@ -65,135 +65,164 @@ X_test = sc.fit_transform(X=X_test)
 # y_test = sc.fit_transform(X=y_test)
 
 
-"""train the linear regression model"""
-linear_regressor = LinearRegression()
-linear_regressor.fit(X=X_train, y=y_train)
-
-reg_training_pred = linear_regressor.predict(X=X_train)
-reg_test_pred = linear_regressor.predict(X=X_test)
-
-
-"""print the linear regression model results"""
-print("-------------------------------")
-# print("linear reg RMSE on training data: %.3f" % sqrt(mean_squared_error(y_true=y_train, y_pred=reg_training_pred)))
-# print("linear reg RMSE on test data: %.3f" % sqrt(mean_squared_error(y_true=y_test, y_pred=reg_test_pred)))
-print("-------------")
-print("linear reg MAE on training data: %.3f" % mean_absolute_error(y_true=y_train, y_pred=reg_training_pred))
-print("linear reg MAE on test data: %.3f" % mean_absolute_error(y_true=y_test, y_pred=reg_test_pred))
-print("-------------")
-print("linear reg variance score on training data: %.3f" % r2_score(y_true=y_train, y_pred=reg_training_pred))
-print("linear reg variance score on test data: %.3f" % r2_score(y_true=y_test, y_pred=reg_test_pred))
-print("-------------------------------")
-
-
-"""train the multi-layer perceptron model"""
-mlp = MLPRegressor(hidden_layer_sizes=(20, 11, 11, 7, 7, 3, 3,), max_iter=3000, n_iter_no_change=300,
-                   activation='relu', solver='adam', verbose=False, warm_start=True)
-mlp.fit(X=X_train, y=y_train)
-
-mlp_training_pred = mlp.predict(X=X_train)
-mlp_test_pred = mlp.predict(X=X_test)
-
-
-"""print the MLP model results"""
-print("-------------------------------")
-# print("mlp RMSE on training data: %.3f" % sqrt(mean_squared_error(y_true=y_train, y_pred=mlp_training_pred)))
-# print("mlp RMSE on test data: %.3f" % sqrt(mean_squared_error(y_true=y_test, y_pred=mlp_test_pred)))
-print("-------------")
-print("mlp MAE on training data: %.3f" % mean_absolute_error(y_true=y_train, y_pred=mlp_training_pred))
-print("mlp MAE on test data: %.3f" % mean_absolute_error(y_true=y_test, y_pred=mlp_test_pred))
-print("-------------")
-print("mlp variance score on training data: %.3f" % r2_score(y_true=y_train, y_pred=mlp_training_pred))
-print("mlp variance score on test data: %.3f" % r2_score(y_true=y_test, y_pred=mlp_test_pred))
-print("-------------------------------")
-
-
-"""train the random forest ensemble model"""
-random_forest = RandomForestRegressor(n_estimators=200, criterion="mae", warm_start=False)
-random_forest.fit(X=X_train, y=y_train)
-
-rf_train_pred = random_forest.predict(X=X_train)
-rf_test_pred = random_forest.predict(X=X_test)
+"""define the linear regression model"""
+# linear_regressor = LinearRegression()
 
 
 """do the KFold cross-validation both with MAE values and r2 scores criteria"""
-cv = ShuffleSplit(n_splits=10, test_size=0.5, random_state=0)
-mae_values = cross_val_score(estimator=random_forest, X=X, y=y, cv=cv, scoring='neg_mean_absolute_error', n_jobs=2)
-r2_scores = cross_val_score(estimator=random_forest, X=X, y=y, cv=cv, scoring='r2', n_jobs=2)
-print("the average MAE values (bias) is:", mae_values.mean())
-print("the std deviation of MAE values (variance) is:", mae_values.std())
-print("the average r2 scores (bias) is:", r2_scores.mean())
-print("the std deviation of r2 scores (variance) is:", r2_scores.std())
+# cv = ShuffleSplit(n_splits=10, test_size=0.5, random_state=2)
+# mae_values = cross_val_score(estimator=linear_regressor, X=X, y=y, cv=cv, scoring='neg_mean_absolute_error', n_jobs=2)
+# r2_scores = cross_val_score(estimator=linear_regressor, X=X, y=y, cv=cv, scoring='r2', n_jobs=2)
+# print("\n ------ Linear Regression ------")
+# print("average MAE values (bias) is:", abs(round(number=mae_values.mean(), ndigits=3)))
+# print("std deviation of MAE values (variance) is:", round(number=mae_values.std(), ndigits=3))
+# best_mae = sorted(mae_values, reverse=False)[-1]
+# print("best MAE value is:", abs(round(number=best_mae, ndigits=3)))
+#
+# print("average r2 scores (bias) is:", round(number=r2_scores.mean() * 100, ndigits=3))
+# print("std deviation of r2 scores (variance) is:", round(number=r2_scores.std() * 100, ndigits=3))
+# best_r2 = sorted(r2_scores, reverse=False)[-1] * 100
+# print("best r2 score is:", round(number=best_r2, ndigits=3))
 
 
-"""print the RF model results"""
-print("-------------------------------")
-# print("rf RMSE on training data: %.3f" % sqrt(mean_squared_error(y_true=y_train, y_pred=rf_train_pred)))
-# print("rf RMSE on test data: %.3f" % sqrt(mean_squared_error(y_true=y_test, y_pred=rf_test_pred)))
-print("-------------")
-print("rf MAE on training data: %.3f" % mean_absolute_error(y_true=y_train, y_pred=rf_train_pred))
-print("rf MAE on test data: %.3f" % mean_absolute_error(y_true=y_test, y_pred=rf_test_pred))
-print("-------------")
-print("rf variance score on training data: %.3f" % r2_score(y_true=y_train, y_pred=rf_train_pred))
-print("rf variance score on test data: %.3f" % r2_score(y_true=y_test, y_pred=rf_test_pred))
-print("-------------------------------")
+"""train the linear regression model and print the results on the never-seen-before test data"""
+# linear_regressor.fit(X=X_train, y=y_train)
+# reg_training_pred = linear_regressor.predict(X=X_train)
+# reg_test_pred = linear_regressor.predict(X=X_test)
+# # print("linear reg RMSE on training data: %.3f" % sqrt(mean_squared_error(y_true=y_train, y_pred=reg_training_pred)))
+# # print("linear reg RMSE on test data: %.3f" % sqrt(mean_squared_error(y_true=y_test, y_pred=reg_test_pred)))
+# print("-------------")
+# print("linear reg MAE on training data: %.3f" % mean_absolute_error(y_true=y_train, y_pred=reg_training_pred))
+# print("linear reg MAE on test data: %.3f" % mean_absolute_error(y_true=y_test, y_pred=reg_test_pred))
+# print("-------------")
+# print("linear reg variance score on training data: %.3f" % r2_score(y_true=y_train, y_pred=reg_training_pred))
+# print("linear reg variance score on test data: %.3f" % r2_score(y_true=y_test, y_pred=reg_test_pred))
+# print("-------------------------------")
 
 
-"""train the ada-boost ensemble model"""
-ada_boost = AdaBoostRegressor(n_estimators=350, learning_rate=1.)
-ada_boost.fit(X=X_train, y=y_train)
-
-ab_train_pred = ada_boost.predict(X=X_train)
-ab_test_pred = ada_boost.predict(X=X_test)
+"""define the multi-layer perceptron model"""
+mlp = MLPRegressor(hidden_layer_sizes=(20, 11, 11, 7, 7, 3, 3,), max_iter=4000, n_iter_no_change=30,
+                   activation='relu', solver='adam', verbose=False, warm_start=False)
 
 
-"""print this model results"""
-print("-------------------------------")
-# print("ada-boost RMSE on training data: %.3f" % sqrt(mean_squared_error(y_true=y_train, y_pred=ab_train_pred)))
-# print("ada-boost RMSE on test data: %.3f" % sqrt(mean_squared_error(y_true=y_test, y_pred=ab_test_pred)))
-print("-------------")
-print("ada-boost MAE on training data: %.3f" % mean_absolute_error(y_true=y_train, y_pred=ab_train_pred))
-print("ada-boost MAE on test data: %.3f" % mean_absolute_error(y_true=y_test, y_pred=ab_test_pred))
-print("-------------")
-print("ada-boost variance score on training data: %.3f" % r2_score(y_true=y_train, y_pred=ab_train_pred))
-print("ada-boost variance score on test data: %.3f" % r2_score(y_true=y_test, y_pred=ab_test_pred))
-print("-------------------------------")
+"""do the KFold cross-validation both with MAE values and r2 scores criteria"""
+cv = ShuffleSplit(n_splits=10, test_size=0.5, random_state=2)
+mae_values = cross_val_score(estimator=mlp, X=X, y=y, cv=cv, scoring='neg_mean_absolute_error', n_jobs=2)
+r2_scores = cross_val_score(estimator=mlp, X=X, y=y, cv=cv, scoring='r2', n_jobs=2)
+print("\n ------ MLP ------")
+print(mae_values)
+print("average MAE values (bias) is:", abs(round(number=mae_values.mean(), ndigits=3)))
+print("std deviation of MAE values (variance) is:", round(number=mae_values.std(), ndigits=3))
+best_mae = sorted(mae_values, reverse=False)[-1]
+print("best MAE value is:", abs(round(number=best_mae, ndigits=3)))
+
+print("average r2 scores (bias) is:", round(number=r2_scores.mean() * 100, ndigits=3))
+print("std deviation of r2 scores (variance) is:", round(number=r2_scores.std() * 100, ndigits=3))
+best_r2 = sorted(r2_scores, reverse=False)[-1] * 100
+print("best r2 score is:", round(number=best_r2, ndigits=3))
 
 
-"""plot driving range based on the battery quantity"""
-quantity = X[:, 2]
-distance = y
-quantity = np.reshape(quantity, newshape=(-1, 1))
-distance = np.reshape(distance, newshape=(-1, 1))
-
-quantity_linear_reg = LinearRegression()
-quantity_linear_reg.fit(X=quantity, y=distance)
-q_slope = quantity_linear_reg.coef_[0]
-q_intercept = quantity_linear_reg.intercept_
-q_predicted_distances = q_intercept + q_slope * quantity
-
-plt.scatter(x=quantity, y=distance, s=15, c='black', linewidths=0.1)
-plt.plot(quantity, q_predicted_distances, c='red', linewidth=2)
-plt.legend(('fitted line', 'data records'), loc='lower right')
-plt.title(label='Linear Regression Plot')
-plt.xlabel(xlabel='quantity (kWh)'), plt.ylabel(ylabel='driving range (km)')
-plt.show()
+"""train the MLP model and print the results on the never-seen-before test data"""
+# mlp.fit(X=X_train, y=y_train)
+# mlp_training_pred = mlp.predict(X=X_train)
+# mlp_test_pred = mlp.predict(X=X_test)
+# # print("mlp RMSE on training data: %.3f" % sqrt(mean_squared_error(y_true=y_train, y_pred=mlp_training_pred)))
+# # print("mlp RMSE on test data: %.3f" % sqrt(mean_squared_error(y_true=y_test, y_pred=mlp_test_pred)))
+# print("-------------")
+# print("mlp MAE on training data: %.3f" % mean_absolute_error(y_true=y_train, y_pred=mlp_training_pred))
+# print("mlp MAE on test data: %.3f" % mean_absolute_error(y_true=y_test, y_pred=mlp_test_pred))
+# print("-------------")
+# print("mlp variance score on training data: %.3f" % r2_score(y_true=y_train, y_pred=mlp_training_pred))
+# print("mlp variance score on test data: %.3f" % r2_score(y_true=y_test, y_pred=mlp_test_pred))
+# print("-------------------------------")
 
 
-"""plot driving range based on the average speed"""
-avg_speed = X[:, 9]
-avg_speed = np.reshape(avg_speed, newshape=(-1, 1))
+"""train the random forest ensemble model"""
+# random_forest = RandomForestRegressor(n_estimators=200, criterion="mae", warm_start=False)
+# random_forest.fit(X=X_train, y=y_train)
+#
+# rf_train_pred = random_forest.predict(X=X_train)
+# rf_test_pred = random_forest.predict(X=X_test)
 
-speed_linear_reg = LinearRegression()
-speed_linear_reg.fit(X=avg_speed, y=distance)
-s_slope = speed_linear_reg.coef_[0]
-s_intercept = speed_linear_reg.intercept_
-s_predicted_distances = s_intercept + s_slope * quantity
 
-plt.scatter(x=avg_speed, y=distance, s=15, c='orange', linewidths=0.1)
-plt.plot(quantity, s_predicted_distances, c='blue', linewidth=2)
-plt.legend(('fitted line', 'data records'), loc='upper left')
-plt.title(label='Linear Regression Plot')
-plt.xlabel(xlabel='average speed (km/h)'), plt.ylabel(ylabel='driving range (km)')
-plt.xlim(-5, 110), plt.ylim(-30, 650)
-plt.show()
+# """do the KFold cross-validation both with MAE values and r2 scores criteria"""
+# cv = ShuffleSplit(n_splits=5, test_size=0.5, random_state=0)
+# mae_values = cross_val_score(estimator=random_forest, X=X, y=y, cv=cv, scoring='neg_mean_absolute_error', n_jobs=2)
+# r2_scores = cross_val_score(estimator=random_forest, X=X, y=y, cv=cv, scoring='r2', n_jobs=2)
+# print("the average MAE values (bias) is:", mae_values.mean())
+# print("the std deviation of MAE values (variance) is:", mae_values.std())
+# print("the average r2 scores (bias) is:", r2_scores.mean())
+# print("the std deviation of r2 scores (variance) is:", r2_scores.std())
+
+
+# """print the RF model results"""
+# print("-------------------------------")
+# # print("rf RMSE on training data: %.3f" % sqrt(mean_squared_error(y_true=y_train, y_pred=rf_train_pred)))
+# # print("rf RMSE on test data: %.3f" % sqrt(mean_squared_error(y_true=y_test, y_pred=rf_test_pred)))
+# print("-------------")
+# print("rf MAE on training data: %.3f" % mean_absolute_error(y_true=y_train, y_pred=rf_train_pred))
+# print("rf MAE on test data: %.3f" % mean_absolute_error(y_true=y_test, y_pred=rf_test_pred))
+# print("-------------")
+# print("rf variance score on training data: %.3f" % r2_score(y_true=y_train, y_pred=rf_train_pred))
+# print("rf variance score on test data: %.3f" % r2_score(y_true=y_test, y_pred=rf_test_pred))
+# print("-------------------------------")
+
+
+# """train the ada-boost ensemble model"""
+# ada_boost = AdaBoostRegressor(n_estimators=350, learning_rate=1.)
+# ada_boost.fit(X=X_train, y=y_train)
+#
+# ab_train_pred = ada_boost.predict(X=X_train)
+# ab_test_pred = ada_boost.predict(X=X_test)
+#
+#
+# """print this model results"""
+# print("-------------------------------")
+# # print("ada-boost RMSE on training data: %.3f" % sqrt(mean_squared_error(y_true=y_train, y_pred=ab_train_pred)))
+# # print("ada-boost RMSE on test data: %.3f" % sqrt(mean_squared_error(y_true=y_test, y_pred=ab_test_pred)))
+# print("-------------")
+# print("ada-boost MAE on training data: %.3f" % mean_absolute_error(y_true=y_train, y_pred=ab_train_pred))
+# print("ada-boost MAE on test data: %.3f" % mean_absolute_error(y_true=y_test, y_pred=ab_test_pred))
+# print("-------------")
+# print("ada-boost variance score on training data: %.3f" % r2_score(y_true=y_train, y_pred=ab_train_pred))
+# print("ada-boost variance score on test data: %.3f" % r2_score(y_true=y_test, y_pred=ab_test_pred))
+# print("-------------------------------")
+#
+#
+# """plot driving range based on the battery quantity"""
+# quantity = X[:, 2]
+# distance = y
+# quantity = np.reshape(quantity, newshape=(-1, 1))
+# distance = np.reshape(distance, newshape=(-1, 1))
+#
+# quantity_linear_reg = LinearRegression()
+# quantity_linear_reg.fit(X=quantity, y=distance)
+# q_slope = quantity_linear_reg.coef_[0]
+# q_intercept = quantity_linear_reg.intercept_
+# q_predicted_distances = q_intercept + q_slope * quantity
+#
+# plt.scatter(x=quantity, y=distance, s=15, c='black', linewidths=0.1)
+# plt.plot(quantity, q_predicted_distances, c='red', linewidth=2)
+# plt.legend(('fitted line', 'data records'), loc='lower right')
+# plt.title(label='Linear Regression Plot')
+# plt.xlabel(xlabel='quantity (kWh)'), plt.ylabel(ylabel='driving range (km)')
+# plt.show()
+#
+#
+# """plot driving range based on the average speed"""
+# avg_speed = X[:, 9]
+# avg_speed = np.reshape(avg_speed, newshape=(-1, 1))
+#
+# speed_linear_reg = LinearRegression()
+# speed_linear_reg.fit(X=avg_speed, y=distance)
+# s_slope = speed_linear_reg.coef_[0]
+# s_intercept = speed_linear_reg.intercept_
+# s_predicted_distances = s_intercept + s_slope * quantity
+#
+# plt.scatter(x=avg_speed, y=distance, s=15, c='orange', linewidths=0.1)
+# plt.plot(quantity, s_predicted_distances, c='blue', linewidth=2)
+# plt.legend(('fitted line', 'data records'), loc='upper left')
+# plt.title(label='Linear Regression Plot')
+# plt.xlabel(xlabel='average speed (km/h)'), plt.ylabel(ylabel='driving range (km)')
+# plt.xlim(-5, 110), plt.ylim(-30, 650)
+# plt.show()
